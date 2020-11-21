3291484ee34cef959ea164f895be8daca5963fa2
Łukasz Bacik <>
2020-11-21 09:53:13 +0100

    amendments in rest api

--- src/lfortune/daemon/app.py
+++ src/lfortune/daemon/app.py
@@ -1,5 +1,8 @@
 
 from flask import Flask, jsonify
+
+from ..cli.cli import get_config_values
+from ..fortune.config import Config
 from ..fortune.factory import Factory
 from ..cli.arguments import parse
 
@@ -7,15 +10,16 @@ from ..cli.arguments import parse
 app = Flask(__name__)
 
 args = parse()
-fortune = Factory.create(args.config)
+config = Config(args.config)
+config_values = get_config_values(args, config)
+
+fortune = Factory.create(config_values)
 
 
 @app.route("/", methods=['GET'])
 def home() -> str:
     fortune_str = fortune.get()
-    # paramater ?raw
     return jsonify({'fortune': fortune_str})
 
-
-# because we use uwsgi
+# commented out because of uwsgi
 # app.run(port=5000)
